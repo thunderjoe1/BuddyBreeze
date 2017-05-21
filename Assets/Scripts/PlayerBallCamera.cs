a4e5569ba54a8fd6fcf7524aa63654738ad23c08
Thunder Clonch <thunderjoe1000@gmail.com>
2017-05-20 23:49:37 -0400

    The ball moves, camera follows it.
    Really basic, needs a lot of work. Add ability to rotate camera and make
    the ball move based on camera rotation.

---
+++ Assets/Scripts/PlayerBallCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBallCamera : MonoBehaviour
+{
+	Transform currentPosition;
+
+	public GameObject playerBall;
+	//public Vector3 cameraPosition;
+
+	void Start ()
+	{
+		currentPosition = transform;
+	}
+
+	void Update ()
+	{
+		currentPosition.transform.position = playerBall.transform.position;
+	}
+}
---
+++ Assets/Scripts/PlayerBallMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBallMove : MonoBehaviour 
+{
+	private Rigidbody rb; //The player's rigidbody.
+
+	float moveHorizontal;
+	float moveVertical;
+	Vector3 movement; //The directions the object is moving in.
+
+	public float moveSpeed; //Multiplier to the magnitude at which ball form adds forces to itself.
+
+	void Start ()
+	{
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () 
+	{
+		moveHorizontal = Input.GetAxis ("Horizontal");
+		moveVertical = Input.GetAxis ("Vertical");
+
+		movement = new Vector3 (moveHorizontal , 0.0f , moveVertical);
+
+		rb.AddForce(movement * moveSpeed);
+	}
+}

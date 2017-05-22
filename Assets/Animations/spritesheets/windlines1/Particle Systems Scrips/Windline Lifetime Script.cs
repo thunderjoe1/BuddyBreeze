a59bbf62800e50bdfa9c7b7b34827f5fe2bdcf5e
Hotel Bones <kiefferwilsonisonfire@gmail.com>
2017-05-21 20:13:47 -0400

    Particle system and organization began
    The sprite sheets for windlines are implemented in the particle system and the level prefab is better organized.

---
+++ Assets/Animations/spritesheets/windlines1/Particle Systems Scrips/Windline Lifetime Script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindlineLifetimeScript : MonoBehaviour {
+
+
+	void Awake()
+	{
+		ParticleSystem.Particle.lifetime = .5f;    
+	}
+}
